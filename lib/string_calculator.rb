4fe45fa1b10d27ca10a71d529246386bda468111
Deborah Peterson <peterson.deb@comcast.net>
2014-11-08 10:26:41 -0700

    support empty-string input for StringCalculator#sum

---
+++ lib/string_calculator.rb
@@ -0,0 +1,8 @@
+class StringCalculator
+
+	def sum(addends)
+		0
+
+	end
+
+end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,8 +1,12 @@
 require 'rspec'
+require 'string_calculator'
 
 describe StringCalculator do 
-	it 'fails' do
-		fail
-	end|
+	it 'returns 0 when given an empty string' do
+		calculator = StringCalculator.new
+		result = calculator.sum("")
+
+		expect(result).to eq(0)
+	end
 	
 end
